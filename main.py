7db9e7d0050d004ada02324a3a28b794070e5c52
myf950904 <121151007+myf950904@users.noreply.github.com>
2022-12-21 15:03:13 +0800

    Update main.py
    调整取今天温度上的错误

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), weather['low']+'° ~'+ weather['high']+'° ', weather['wind']
+  temp_day = str(weather['low']) +"° ~"+ str(weather['high']) +"° "
+  return weather['weather'], math.floor(weather['temp']), temp_day, weather['wind']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
